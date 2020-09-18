8ccec3c94628a7a79bf4fbf1f80b2e8da45b67b1
RishabhVaishnavProjects <33623690+RishabhVaishnavProjects@users.noreply.github.com>
2020-09-18 10:00:28 -0700

    Update jquery_library_for_modal.js

--- js/jquery_library_for_modal.js
+++ js/jquery_library_for_modal.js
@@ -1,7 +1,7 @@
-if ("undefined" == typeof jQuery) throw new Error("Bootstrap's JavaScript requires jQuery"); + function (a) {
+if ("undefined" == typeof jQuery) throw new Error("CSS for pop-up requires jQuery"); + function (a) {
     "use strict";
     var b = a.fn.jquery.split(" ")[0].split(".");
-    if (b[0] < 2 && b[1] < 9 || 1 == b[0] && 9 == b[1] && b[2] < 1 || b[0] > 2) throw new Error("Bootstrap's JavaScript requires jQuery version 1.9.1 or higher, but lower than version 3")
+    if (b[0] < 2 && b[1] < 9 || 1 == b[0] && 9 == b[1] && b[2] < 1 || b[0] > 2) throw new Error("Required jQuery version is 1.9.1 or higher, but lower than version 3")
 }(jQuery), + function (a) {
     "use strict";
 
